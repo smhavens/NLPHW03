2dd4d1f843d10434f91aca18e9a1c1742dfbe06f
smhavens <smhavens@umich.edu>
2023-11-05 22:30:39 -0500

    Model created with embeddings.

--- word_embedding.py
+++ word_embedding.py
@@ -2,6 +2,7 @@ from datasets import load_dataset
 import shutil
 import json
 from collections import defaultdict
+import multiprocessing
 import gensim
 import gensim.downloader as api
 from gensim import corpora
@@ -15,6 +16,7 @@ import argparse
 import nltk
 import scipy.sparse
 import numpy as np
+import string
 
 
 '''STEPS FOR CODE:
@@ -28,14 +30,72 @@ def train_embeddings():
     '''TRAIN WORD EMBEDDINGS
     This will be making use of the dataset from wikipedia and the first step'''
     dataset = load_dataset("wikipedia", "20220301.simple")
+    cores = multiprocessing.cpu_count()
     # check the first example of the training portion of the dataset :
     # print(dataset['train'][0])
     dataset_size = len(dataset)
-    embeddings_model = Word2Vec(sentences=common_texts, vector_size=100, window=5, min_count=1, workers=4)
+    
+    ### BUILD VOCAB ###
+    # print(type(dataset["train"][0]))
+    vocab = set()
+    vocab_size = 0
+    count = 0
+    ## Generate vocab and split sentances and words?
+    data = []
+    for index, page in enumerate(dataset["train"]):
+        document = page["text"]
+        document = document.replace("\n", ". ")
+        # print(document)
+        for sent in document.split("."):
+            # print("Sentance:", sent)
+            new_sent = []
+            clean_sent =[s for s in sent if s.isalnum() or s.isspace()]
+            clean_sent = "".join(clean_sent)
+            for word in clean_sent.split(" "):
+                if len(word) > 0:
+                    new_word = word.lower()
+                    # print("Word:", new_word)
+                    if new_word[0] not in string.punctuation:
+                        new_sent.append(new_word)
+            if len(new_sent) > 0:
+                data.append(new_sent)
+                # print("New Sent:", new_sent)
+    
+    
+    for index, page in enumerate(dataset["train"]):
+        # print(page["text"])
+        # for text in page:
+        #     print(text)
+        text = page["text"]
+        clean_text = [s for s in text if s.isalnum() or s.isspace()]
+        clean_text = "".join(clean_text)
+        clean_text = clean_text.replace("\n", " ")
+        # text = text.replace('; ', ' ').replace(", ", " ").replace("\n", " ").replace(":", " ").replace(". ", " ").replace("! ", " ").replace("? ", " ").replace()
+        
+        for word in clean_text.split(" "):
+            # print(word)
+            if word != "\n" and word != " " and word not in vocab:
+                vocab.add(word)
+                vocab_size += 1
+            # if index == 10:
+            #     break
+            # print(f"word #{index}/{count} is {word}")
+        count += 1
+            
+    print(f"There are {vocab_size} vocab words")
+    
+    embeddings_model = Word2Vec(
+                     data,
+                     epochs= 10,
+                     window=10,
+                     vector_size= 50)
     embeddings_model.save("word2vec.model")
     
     embeddings_model = Word2Vec.load("word2vec.model")
-    embeddings_model.train(dataset, total_examples=dataset_size, epochs=15)
+    # embeddings_model.train(dataset, total_examples=dataset_size, epochs=15)
+    # print(embeddings_model['train'])
+    # print(embeddings_model.wv["france"])
+    return embeddings_model
 
 
 def compare_embeddings(model):
